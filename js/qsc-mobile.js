ea8e371f335da0ba2352a4cbfa49ee5b35919297
Zeno Zeng <zenoes@qq.com>
2012-11-13 17:12:43 +0800

    修正login

--- js/qsc-mobile.js
+++ js/qsc-mobile.js
@@ -26,17 +26,45 @@ var pwd = localStorage.getItem('pwd') ? localStorage.getItem('pwd') : false;
 var isLogin = localStorage.getItem('isLogin') ? localStorage.getItem('isLogin') : false;
 
 function pleaseLoginIfNotLogin(callback) {
-    if(!isLogin) {
-        $('.mask').hide(200);
+    if(isLogin) {
+        if(typeof(callback) == 'function') {
+            callback();
+        }
+    } else {
+        
         $('#login').show(200);
-        $.include(['qsc-mobile-login.js']);
-    }
-    
-    if(typeof(callback) == 'function') {
-        callback();
+        $.include(['BigInt.js','Barrett.js','RSA.js']);
+        
+        $('#login_submit').click(function(){
+            stuid = $("#stuid").val();
+            
+            var rsa_n = "AA18ABA43B50DEEF38598FAF87D2AB634E4571C130A9BCA7B878267414FAAB8B471BD8965F5C9FC3818485EAF529C26246F3055064A8DE19C8C338BE5496CBAEB059DC0B358143B44A35449EB264113121A455BD7FDE3FAC919E94B56FB9BB4F651CDB23EAD439D6CD523EB08191E75B35FD13A7419B3090F24787BD4F4E1967";
+            setMaxDigits(131); //131 => n的十六进制位数/2+3  
+            var key      = new RSAKeyPair("10001", '', rsa_n); //10001 => e的十六进制  
+            pwd = $("#pwd").val();
+            pwd = encryptedString(key, pwd); //不支持汉字
+            
+            myGetJsonp('validate', function(data) {
+                if(stuid != '') {
+                    localStorage.setItem('stuid', stuid);
+                    localStorage.setItem('pwd', pwd);
+                    localStorage.setItem('isLogin', true);
+                    isLogin = true;
+                    $('#login').slideUp(200);
+                    
+                    // 回调函数
+                    if(typeof(callback) == 'function') {
+                        callback();
+                    }
+                    
+                } else {
+                    localStorage.setItem('isLogin', false);
+                    isLogin = false;
+                }
+            });
+        });
     }
 }
-
 
 function myGetJsonp(name, callback) {
     $.getJSON(siteUrl+'/jsonp/'+name+'?stuid='+stuid+'&pwd='+pwd+'&callback=?', function (data) {
@@ -140,8 +168,8 @@ $(document).ready(function() {
     });
     
     $('#menu .logout').click(function(){
-        var stuid = false;
-        var pwd = false;
+        var stuid = '';
+        var pwd = '';
         var isLogin = false;
         // 清空localStorage
         localStorage.clear();
