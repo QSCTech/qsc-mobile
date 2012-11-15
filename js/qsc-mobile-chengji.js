e54bca01f118b3607dd973ba1b48c590206e95bc
Zeno Zeng <zenoes@qq.com>
2012-11-15 15:49:14 +0800

    gpa

--- js/qsc-mobile-chengji.js
+++ js/qsc-mobile-chengji.js
@@ -1,25 +1,45 @@
 var chengJiData;
 
 function loadChengji(){
+    var chengJiArray = chengJiData['chengji_array'];
+    var gpa = chengJiData['junji_array'];
+    
     var html = '';
-    for(var i = 0; i < chengJiData.length; i++) {
+    for(var i = 0; i < chengJiArray.length; i++) {
         html += '<div class="chengji>';
         html += '<div class="name">';
-        html += chengJiData[i]['课程名称'];
+        html += chengJiArray[i]['课程名称'];
         html += '</div>';
         html += '<div class="score">';
-        html += '成绩：'+chengJiData[i]['成绩'];
+        html += '成绩：'+chengJiArray[i]['成绩'];
         html += '</div>';
         html += '<div class="credit">';
-        html += '学分：'+chengJiData[i]['学分'];
+        html += '学分：'+chengJiArray[i]['学分'];
         html += '</div>';
         html += '<div class="gradepoint">';
-        html += '绩点：'+chengJiData[i]['绩点'];
+        html += '绩点：'+chengJiArray[i]['绩点'];
         html += '</div>';
         html += '</div>';
     }
-    $('#gpa header').click();
     $('#chengji #danke .detail').html(html);
+
+    // gpa
+    var html = '';
+    for(var i = 0; i < gpa.length; i++) {
+        html += '<div class="gpa">';
+        html += '<div class="date">';
+        html += gpa[i]['时间'];
+        html += '</div>';
+        html += '<div class="junji">';
+        html += gpa[i]['均绩'];
+        html += '</div>';
+        html += '<div class="zongxuefen">';
+        html += gpa[i]['总学分'];
+        html += '</div>';
+        html += '</div>';
+    }
+    $('#gpa .detail').html(html);
+    $('#gpa header').click();
 }
 
 if (localStorage.getItem('chengJi')) {
