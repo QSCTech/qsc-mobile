b095f0461dd2dcd6d5a45063a651e017803963eb
Hexcles Ma <bob1211@gmail.com>
2012-11-28 21:41:18 +0800

    little change

--- md5.php
+++ md5.php
@@ -1,9 +1,7 @@
 <?php
 function echo_md5($file) {
-    echo md5_file($file);
-    echo ' ';
-    echo $file;
-    echo "\n";
+    echo md5_file($file) . "  $file\n";
+    return 0;
 }
 
 // root 
@@ -16,8 +14,8 @@ foreach (array('css', 'img', 'js') as $dir) {
     foreach(scandir($dir) as $file) {
         if($file == '.') continue;
         if($file == '..') continue;
-        if(stripos($file, '~') !== FALSE) continue;
-        if(stripos($file, '#') !== FALSE) continue;
+        if(strpos($file, '~') !== FALSE) continue;
+        if(strpos($file, '#') !== FALSE) continue;
 
         echo_md5($dir.'/'.$file);
     }
