c1c80806c34a9a170b1191944d1e4b919e9e043f
Zeno Zeng <zenoes@qq.com>
2012-11-30 17:42:28 +0800

    update get/index.php

--- get/index.php
+++ get/index.php
@@ -1,4 +1,15 @@
 <?php
-//判断useragent
-//如果是android就给出apk的file
-header();
+// 判断useragent
+$ua = _SERVER['HTTP_USER_AGENT'];
+
+if (stripos( $ua, 'Windows Phone' ) !== FALSE) {
+    header('https://build.phonegap.com/apps/245666/download/winphone');
+} elseif (stripos( $ua, 'Apple-iPhone' ) !== FALSE) {
+    
+} elseif (stripos( $ua, 'Apple-iPad' ) !== FALSE) {
+
+} else {
+    header('Location: mobile_QSC.apk');
+}
+
+exit();
