3c866ef95a9a014c9b01a95717a06bf2143e9563
Zeno Zeng <zenoes@qq.com>
2012-12-12 20:19:45 +0800

    不显示过期的考试

--- js/qsc-mobile-kaoshi.js
+++ js/qsc-mobile-kaoshi.js
@@ -8,6 +8,18 @@ function loadKaoShi() {
         if(kaoShiData[i]['考试时间'] == ' ') //考试时间为空
             continue;
 
+        // 不显示过期的考试
+        var time = kaoShiData[i]['考试时间'];
+        time = time.replace(/\(.*$/, '');
+        time = time.replace(/(年|月)/g, '-');
+        time = time.replace(/日/g, '');
+        time = new Date(time+' 23:59');
+
+        var today = new Date();
+
+        if(today.getTime() > time.getTime())
+          continue;
+
         html += '<div class="kaoshi">';
         html += '<div class="name">';
         html += kaoShiData[i]['课程名称'];
