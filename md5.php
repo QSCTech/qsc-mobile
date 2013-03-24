d31b1e68ae097e602b259741b86bc3b6a25314fd
Zeno Zeng <zenoes@qq.com>
2013-03-24 12:05:43 +0800

    update manifest & md5

--- md5.php
+++ md5.php
@@ -1,6 +1,14 @@
 <?php
+if(isset($_REQUEST['hide']) && $_REQUEST['hide'] == 't') {
+    $hide = true;
+}
 function echo_md5($file) {
-    echo md5_file($file) . "  $file\n";
+    global $hide;
+    if($hide) {
+        echo "$file\n";
+    } else {
+        echo md5_file($file) . "  $file\n";
+    }
     return 0;
 }
 
@@ -16,7 +24,13 @@ foreach (array('css', 'img', 'js') as $dir) {
         if($file == '..') continue;
         if(strpos($file, '~') !== FALSE) continue;
         if(strpos($file, '#') !== FALSE) continue;
-
+        if(strpos($file, 'src.js') !== FALSE) continue;
+        if(strpos($file, '.less') !== FALSE) continue;
+        if(strpos($file, '.css') !== FALSE) {
+            if($file !== 'styles.css') {
+                continue;
+            }
+        }
         echo_md5($dir.'/'.$file);
     }
 }
